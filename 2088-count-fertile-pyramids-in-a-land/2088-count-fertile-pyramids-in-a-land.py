272284f9286f7a89b5a65d733901c2f23c19d798
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-08 11:52:14 +0700

    Time: 4751 ms (6.45%), Space: 21.4 MB (28.93%) - LeetHub

--- 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.py
+++ 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.py
@@ -8,10 +8,11 @@ class Solution:
             # DP [height - 1][bottom][start_bottom]
             # Loop over each height
             dp = {}
-            level_one_pyramid = dict()
+            # Current level result
+            current_level = dict()
+            # Loop over size-1 pyramid
             for i in range(m):
-                level_one_pyramid[i] = grid[i]
-            dp[1] = level_one_pyramid
+                current_level[i] = grid[i]
             result = 0
             for h in range(2, mh + 1):
                 l = 2 * h - 1
@@ -25,13 +26,12 @@ class Solution:
                         if row[i]:
                             if i + 1 - start >= l:
                                 left_index = i - l + 1
-                                if dp[h - 1][b_idx - 1][left_index + 1]:
+                                if current_level[b_idx - 1][left_index + 1]:
                                     current_bottom[left_index] = 1 
                                     result += 1
                         else:
                             start = i + 1
                     current_level_pyramid[b_idx] = current_bottom
-                del dp[h - 1]
-                dp[h] = current_level_pyramid
+                current_level = current_level_pyramid
             return result
         return helper(x) + helper(x[::-1])
