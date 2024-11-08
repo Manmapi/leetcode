d2f55982c74831eb748ccdbea234c4d214879a57
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-08 14:34:55 +0700

    Time: 8638 ms (6.45%), Space: 31.2 MB (13.22%) - LeetHub

--- 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.py
+++ 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.py
@@ -5,34 +5,42 @@ class Solution:
         # Max height of pyramidal plot
         mh = min((n + 1) // 2, m)
         def helper(grid):
-            
-            # DP [height - 1][bottom][start_bottom]
-            # Loop over each height
-            dp = {}
+            # DP [row_index][height][start_bottom]
             # Current level result
-            current_level = dict()
+            current_level = defaultdict(dict)
             # Loop over size-1 pyramid
-            for i in range(m):
-                current_level[i] = grid[i]
+            for i in range(n):
+                current_level[1][i] = grid[0][i]
             result = 0
-            for h in range(2, mh + 1):
-                l = 2 * h - 1
-                current_level_pyramid = dict()
-                for b_idx in range(h -1, m):
-                    current_bottom = [0] * n
-                    row = grid[b_idx]
-                    # Find any consecutive 1-array with length 2 * h - 1
-                    start = 0
-                    for i in range(n):
-                        if row[i]:
-                            if i + 1 - start >= l:
-                                left_index = i - l + 1
-                                if current_level[b_idx - 1][left_index + 1]:
-                                    current_bottom[left_index] = 1 
-                                    result += 1
+            for i in range(1, m):
+                current = defaultdict(dict)
+                row = grid[i]
+                # Find any consecutive 1-array with length
+                start = 0
+                for j in range(n):
+                    if row[j]:
+                        current[1][j] = 1
+                        # current[1][j] = 1
+                        l = j + 1 - start
+                        if l % 2 == 0:
+                            start_index = start + 1
                         else:
-                            start = i + 1
-                    current_level_pyramid[b_idx] = current_bottom
-                current_level = current_level_pyramid
+                            start_index = start
+                        for left_index in range(start_index, j - 1, 2):
+                            level = (j - left_index)//2 + 1
+                            if current_level[level - 1].get(left_index + 1, 0):
+                                current[level][left_index] = 1 
+                                result += 1
+                    else:
+                        start = j + 1
+                current_level = current
             return result
-        return helper(x) + helper(x[::-1])
+        result1 = helper(x)
+        print("Result 1", result1)
+        result2 = helper(x[::-1])
+        print("Reusult 2", result2)
+        return result1 + result2
+        # 0 1 1 1 0
+        # 1 1 1 1 1
+        # 1 1 1 1 1 
+        # 1 0 1 0 1
