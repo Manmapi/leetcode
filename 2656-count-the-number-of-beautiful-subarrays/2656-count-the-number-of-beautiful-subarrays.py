6e88e754ac532bab64a44f88026a85076a23e843
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-11 10:03:57 +0700

    Time: 83 ms (99.15%), Space: 38.9 MB (13.12%) - LeetHub

---
+++ 2656-count-the-number-of-beautiful-subarrays/2656-count-the-number-of-beautiful-subarrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def beautifulSubarrays(self, nums: List[int]) -> int:
+        res = 0
+        prefix = [0]
+        for num in nums:
+            prefix.append(num ^ prefix[-1])
+        counter = Counter(prefix)
+        for k, v in counter.items():
+            res += v * (v - 1) // 2
+        return res
