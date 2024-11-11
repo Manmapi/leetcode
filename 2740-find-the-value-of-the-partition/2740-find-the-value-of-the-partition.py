f484ddeee2bb6b8e0f1f00e50e7423d2c6b78575
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-11 17:41:20 +0700

    Time: 77 ms (41.79%), Space: 28.6 MB (85.14%) - LeetHub

---
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findValueOfPartition(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        diff = nums[-1] - nums[0]
+        for i in range(1, n):
+            diff = min(diff, nums[i] - nums[i - 1])
+        return diff
