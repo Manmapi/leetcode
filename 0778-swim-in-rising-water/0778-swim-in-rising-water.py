cfdd6f70d94e70688131c8d1383a9d237fb85fe1
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-12 10:10:09 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,53 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        # Dijkstra
+        n = len(grid)
+        n_2 = n ** 2
+        edges = []
+        max_ = 0
+        vals = []
+        for i in range(n):
+            for j in range(n): 
+                key = i * n + j
+                max_ = max(grid[i][j], max_)
+                vals.append(grid[i][j])
+                val = grid[i][j]
+                if i != 0:
+                    edges.append((key, key - n, max(val, grid[i - 1][j])))
+                if i != n - 1:
+                    edges.append((key, key + n, max(val, grid[i + 1][j])))
+                if j != 0:
+                    edges.append((key, key - 1, max(val, grid[i][j - 1])))
+                if j != n - 1:
+                    edges.append((key, key + 1, max(val, grid[i][j + 1])))
+        vals.sort()
+        def union_find(k):
+            ids = [i for i in range(n_2)]
+            size = [1] * n_2
+            def root(n):
+                while n != ids[n]:
+                    n = ids[n]
+                return n
+            for first, second, cost in edges:
+                if cost > k:
+                    continue
+                root_first = root(first)
+                root_second = root(second)
+                if size[root_second] > size[root_first]:
+                    ids[root_first] = root_second
+                    size[root_second] = size[root_second] + size[root_first]
+                else:
+                    ids[root_second] = root_first
+                    size[root_first] = size[root_first] + size[root_second]
+            return root(0) == root(n_2 - 1)
+        # BS
+        l, r = 0, n_2 - 1
+        while l < r:
+            mid = (l + r) // 2
+            if union_find(vals[mid]):
+                r = mid - 1
+            else:
+                l = mid + 1
+        # print(vals)
+        # print(l)
+        return max(grid[0][0], vals[l])
