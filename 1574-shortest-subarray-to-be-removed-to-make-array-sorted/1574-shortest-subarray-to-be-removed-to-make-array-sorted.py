ec966149d2c89edda1372d66ade1d3270d060a43
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-12 16:06:45 +0700

    Time: 15 ms (65.63%), Space: 30.2 MB (98.93%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,35 +1,26 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
         n = len(arr)
+        left = 1
+        while left < n and arr[left] >= arr[left-1]:
+            left += 1
+        left -= 1
+        right = n - 2
+        while right >= 0 and arr[right] <= arr[right + 1]:
+            right -= 1
+        right += 1
 
-        start = [arr[0]]
-        i = 1
-        while i < n and arr[i] >= start[-1]:
-            start.append(arr[i])
-            i += 1
-
-        end = [arr[-1]]
-        i = n - 2
-        while i >= 0 and arr[i]<= end[-1]:
-            end.append(arr[i])
-            i -= 1
-        end = end[::-1]
-
-        l_start = len(start)
-        l_end = len(end)
-
-        if l_start + l_end > n:
+        if left >= right:
             return 0
-
-        i, j = 0, 0
-        result = 0
-        while i < l_start:
-            val = start[i]
-            while j < l_end and end[j] < val:
-                j += 1
-            res = i + 1 + (l_end - j)
+        result = max(left + 1, n - right)
+        i = 0
+        while i <= left:
+            val = arr[i]
+            while right < n and arr[right] < val:
+                right += 1
+            res = i + 1 + (n - right)
             result = max(res, result)
-            if j == n:
+            if right == n:
                 break
             i += 1
-        return n - max(result, l_end, l_start)
+        return n - result
