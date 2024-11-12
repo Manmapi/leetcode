faa9f19680a36f70d092f00e2f3c688f5d9bea2b
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-12 10:22:27 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,52 +1,36 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # Union find + BS
+        # Dijkstra
         n = len(grid)
         n_2 = n ** 2
-        edges = []
-        vals = []
+        vertexes = {
+            i: [] for i in range(0, n_2)
+        }
         for i in range(n):
             for j in range(n): 
                 key = i * n + j
-                vals.append(grid[i][j])
-                val = grid[i][j]
+                points = []
                 if i != 0:
-                    edges.append((key, key - n, max(val, grid[i - 1][j])))
+                    points.append((key - n, grid[i - 1][j]))
                 if i != n - 1:
-                    edges.append((key, key + n, max(val, grid[i + 1][j])))
+                    points.append((key + n, grid[i + 1][j]))
                 if j != 0:
-                    edges.append((key, key - 1, max(val, grid[i][j - 1])))
+                    points.append((key - 1, grid[i][j - 1]))
                 if j != n - 1:
-                    edges.append((key, key + 1, max(val, grid[i][j + 1])))
-        vals.sort()
-        def union_find(k):
-            ids = [i for i in range(n_2)]
-            size = [1] * n_2
-            def root(n):
-                while n != ids[n]:
-                    n = ids[n]
-                return n
-            for first, second, cost in edges:
-                if cost > k:
-                    continue
-                root_first = root(first)
-                root_second = root(second)
-                if size[root_second] > size[root_first]:
-                    ids[root_first] = root_second
-                    size[root_second] = size[root_second] + size[root_first]
-                else:
-                    ids[root_second] = root_first
-                    size[root_first] = size[root_first] + size[root_second]
-            return root(0) == root(n_2 - 1)
-        # BS
-        l = 0
-        while vals[l] < grid[0][0] or vals[l] < grid[n-1][n-1]:
-            l += 1
-        r = n_2 - 1
-        while l <= r:
-            mid = (l + r) // 2
-            if union_find(vals[mid]):
-                r = mid - 1
-            else:
-                l = mid + 1
-        return vals[l]
+                    points.append((key + 1, grid[i][j + 1]))
+                vertexes[key] = points
+        # Distance, index
+        dist = [(grid[0][0], 0)]
+        visited = set()
+        while dist:
+            cost, index = heapq.heappop(dist)
+            # i, j = index // n, index % n
+            if index == n_2 - 1:
+                return max(cost, grid[n - 1][n - 1])
+            visited.add(index)
+            for p, c in vertexes[index]:
+                if p not in visited:
+                    heapq.heappush(dist, (max(cost, c), p))
+        return 0
+        # 3 2 
+        # 0 1
