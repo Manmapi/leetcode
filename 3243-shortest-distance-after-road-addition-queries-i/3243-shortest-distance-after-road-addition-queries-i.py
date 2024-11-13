9c6ec8ed14d5c12010b34d4ddecbdcbc05b0912f
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-13 10:34:20 +0700

    Time: 98 ms (96.41%), Space: 17 MB (67.73%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,43 +1,27 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         edge_map = {i: [i +1] if i != n - 1 else [] for i in range(n)}
-        history_map = {}
+        history = [i for i in range(n)]
         result = [n]
-
-        def bfs(start):
-            q = []
-            if not history_map:
-                dist = 0
-                q.append(0)
-            else:
-                if history_map.get(start) is None or history_map[start] > result[-1]:
-                    return result[-1]
-                dist = history_map[start]
-                for k, v in history_map.items():
-                    if v == dist:
-                        q.append(k)
-            visited = set(q)
+        def bfs(start, dist):
+            q = [start]
             while q:      
                 q_new = []
                 for node in q:
-                    history_map[node] = min(dist, history_map.get(node, node))
-                    
+                    history[node] = min(dist, history[node])
                     if node == n -1:
                         return dist
                     next_nodes = edge_map[node]
                     for next_node in next_nodes:
-                        if next_node not in visited:
-                            visited.add(next_node)
+                        if history[next_node] > dist:                            
                             q_new.append(next_node)
                 dist += 1
                 q = q_new
-
-        bfs(0)
+            return result[-1]
         for x, y in queries:
             edge_map[x].append(y)
-            if history_map[y] > history_map[x] + 1:
-                result.append(bfs(x))
+            if history[y] > history[x] + 1:
+                result.append(bfs(y, history[x] + 1))
             else:
                 result.append(result[-1])        
         return result[1:]
-        
