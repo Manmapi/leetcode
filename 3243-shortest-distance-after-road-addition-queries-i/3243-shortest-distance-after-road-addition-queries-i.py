87e2574d9bd63fa1858cd3589b4f7cd074a63ae1
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-13 09:52:34 +0700

    Time: 441 ms (76.65%), Space: 16.9 MB (89.24%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,28 +1,44 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         edge_map = {i: [i +1] if i != n - 1 else [] for i in range(n)}
-        def bfs():
-            q = deque()
-            q.append(0)
-            visited = set()
-            dist = 0
-            while q:                
+        
+        history_map = {}
+        result = [n]
+        # Restore bfs?????
+        def bfs(start):
+            nonlocal history_map
+            
+            q = []
+            if not history_map:
+                dist = 0
+                q.append(0)
+            else:
+                if history_map.get(start) is None or history_map[start] > result[-1]:
+                    return result[-1]
+                dist = history_map[start]
+                for k, v in history_map.items():
+                    if v == dist:
+                        q.append(k)
+            visited = set(q)
+            while q:      
                 q_new = []
                 for node in q:
-                    visited.add(node)
+                    history_map[node] = min(dist, history_map.get(node, node))
+                    
                     if node == n -1:
                         return dist
                     next_nodes = edge_map[node]
                     for next_node in next_nodes:
                         if next_node not in visited:
+                            visited.add(next_node)
                             q_new.append(next_node)
                 dist += 1
                 q = q_new
-        result = []
+        bfs(0)
         for x, y in queries:
             edge_map[x].append(y)
-            result.append(bfs())
-        return result
+            result.append(bfs(x))        
+        return result[1:]
         
 
             
