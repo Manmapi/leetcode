eba9bde15b56068b05387b196921505bb47227d5
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-13 09:41:53 +0700

    Time: 1654 ms (22.16%), Space: 17.1 MB (14.83%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,26 +1,22 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        # table = [[0] * n for _ in range(n)]
-        # for i in range(n - 1):
-        #     table[i][i + 1] = 1
-        # for x, y in queries:
-        #     table[x][y] = 1
         edge_map = {i: [i +1] if i != n - 1 else [] for i in range(n)}
-        # @lru_cache(None)
         def bfs():
             q = deque()
-            q.append([0, 0])
+            q.append(0)
             visited = set()
-            while q:
+            dist = 0
+            while q:                
                 q_new = []
-                for node, dist in q:
+                for node in q:
                     visited.add(node)
                     if node == n -1:
                         return dist
                     next_nodes = edge_map[node]
                     for next_node in next_nodes:
                         if next_node not in visited:
-                            q_new.append([next_node, dist + 1])
+                            q_new.append(next_node)
+                dist += 1
                 q = q_new
         result = []
         for x, y in queries:
