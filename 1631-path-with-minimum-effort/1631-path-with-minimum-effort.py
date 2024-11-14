5fc4e40a2fc28baea5474c2555ce31cf84bd7eea
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-14 10:13:48 +0700

    Time: 365 ms (47.57%), Space: 22.2 MB (5.33%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -21,13 +21,12 @@ class Solution:
         while q:
             weight, i, j = heapq.heappop(q)
             score = heights[i][j]
-            # if i == n - 1 and j == m - 1:
-            #     break
+            if i == n - 1 and j == m - 1:
+                break
             for x, y in vertex[(i, j)]:
                 val = max(weight, abs(score - heights[x][y]))
                 if val < dist[x][y]:
                     dist[x][y] = val
                     # visited.add((x, y))
                     heapq.heappush(q, (val, x, y))
-        print(dist)
         return dist[-1][-1]
