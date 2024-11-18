3e2830040227668472bb9c799d68f484e3d54e54
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-18 11:34:22 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,33 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        if k == 0:
+            return 0
+        n = len(s)
+        right = defaultdict(lambda: {0: n})
+        a = b = c = 0
+        for i in range(n - 1, -1, -1):
+            if s[i] == "a":
+                a += 1
+                right[s[i]][a] = i
+            elif s[i] == "b":
+                b += 1
+                right[s[i]][b] = i
+            else:
+                c += 1
+                right[s[i]][c] = i
+        if any([a < k, b < k, c < k]):
+            return - 1
+        a = b = c = 0
+        res = n
+        for i in range(n):
+            if s[i] == "a":
+                a += 1
+            elif s[i] == "b":
+                b += 1
+            else:
+                c += 1
+            val =  min(
+                right["a"][max(k - a, 0)], right["b"][max(k - b, 0)], right["c"][max(k - c, 0)]
+            )
+            res = min(res, i + 1 + (n - val))
+        return res
