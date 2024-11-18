2f2ff62d863bc7ea10f924e470fa05dbd0efc082
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-18 10:58:03 +0700

    Time: 2338 ms (21.59%), Space: 42.5 MB (60.71%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -2,17 +2,6 @@ class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        # vertex = defaultdict(list)
-        # for i in range(n):
-        #     for j in range(m):
-        #         if i != 0:
-        #             vertex[(i, j)].append([(i - 1, j), grid[i - 1][j]])    
-        #         if i != n - 1:
-        #             vertex[(i, j)].append([(i + 1, j), grid[i + 1][j]])
-        #         if j != 0:
-        #             vertex[(i, j)].append([(i, j - 1), grid[i][j - 1]])
-        #         if j != m - 1:
-        #             vertex[(i, j)].append([(i, j + 1), grid[i][j + 1]])
         cost = [[float("inf")] * m for _ in range(n)]
         cost[0][0] = 0
         q = set([(0, 0)])
