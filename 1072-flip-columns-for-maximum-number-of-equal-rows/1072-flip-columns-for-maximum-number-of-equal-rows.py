a0c4b76a2ba78263febe2fa799f6e6701d1c297c
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-21 16:14:34 +0700

    Time: 50 ms (81.52%), Space: 19.4 MB (38.62%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -5,11 +5,12 @@ class Solution:
         counter = dict()
         max_ = 2 ** n - 1
         h = 2 ** (n - 1) - 1
+        power = [2 ** i for i in range(n)]
         for i in range(m):
             val = 0
             for j in range(n):
                 if matrix[i][j]:
-                    val += 2 ** j
+                    val += power[j]
             if val > h:
                 val = max_ - val
             counter[val] = counter.get(val, 0) + 1
