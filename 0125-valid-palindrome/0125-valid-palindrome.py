bfd0c41cc563742c59bd51bf9c89c11e920da493
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-26 09:25:45 +0700

    Time: 64 ms (5.26%), Space: 17.6 MB (30.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,5 +3,6 @@ class Solution:
         # 48 - 57 
         # 97 - 122
         s = s.lower()
-        s = [_s for _s in s if _s.isalnum()]
+        accept_range = [*range(48, 58), *range(97, 123)]
+        s = [_s for _s in s if ord(_s) in accept_range]
         return s == s[::-1]
