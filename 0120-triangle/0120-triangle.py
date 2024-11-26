bceb2a4c0f3687390fe56fdd25e4b05b26ef7407
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-11-26 09:48:24 +0700

    Time: 8 ms (28.85%), Space: 18.8 MB (17.47%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,12 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        @lru_cache(None)
+        cache = defaultdict(dict)
         def helper(row, index):
+            if cache[row].get(index) is not None:
+                return cache[row][index]
             if row == n:
                 return 0
-            return min(
+            result =  min(
                 triangle[row][index] + helper(row + 1, index),
                 triangle[row][index + 1] + helper(row + 1, index + 1)
             )
+            cache[row][index] = result
+            return result
         return triangle[0][0] + helper(1, 0)
