8448f01b3aff9e6b23901e4bac652c88c4363ac6
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-03 17:50:08 +0700

    Time: 0 ms (100%), Space: 17.2 MB (6.43%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        result = ""
+        n = len(s)
+        i = 0
+        while i < n:
+            c = s[i]
+            if c.isdigit():
+                number = c
+                while s[i + 1] != "[":
+                    number += s[i + 1]
+                    i += 1
+                i += 2
+                count = int(number)
+                stack_count = 1
+                sub_s = "["
+                while stack_count > 0:
+                    a = s[i]
+                    if a == "[":
+                        stack_count += 1
+                    elif a == "]":
+                        stack_count -= 1
+                    i += 1
+                    sub_s += a
+                sub_result = self.decodeString(sub_s[1:-1])
+                result += count * sub_result
+                i -= 1
+
+            else:
+                result += c
+            i += 1
+        return result
