41da2bee1c25166baa86d505f9a363c3e2021fe2
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-04 13:24:46 +0700

    Time: 1166 ms (5.49%), Space: 56 MB (6.2%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.py
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -1,23 +1,28 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        n = len(nums)
         l = 0
-        map_ = dict()
-        pre = nums[0] & 1
-        for i in range(1, n):
-            val = nums[i] & 1
-            if not pre ^ val:
-                for j in range(l, i):
-                    map_[j] = i - 1
+        previous = nums[0]
+        p = []
+        for i in range(1, len(nums)):
+            if (nums[i]+previous) % 2 == 0:
+                p.append([l,i-1])
                 l = i
-            pre = val
-        if l != n - 1:
-            for j in range(l, n):
-                map_[j] = n - 1
-        result = []
-        for start, end in queries:
-            if map_.get(start, start) < end:
-                result.append(False)
+            previous = nums[i]
+        p.append([l,len(nums)-1])
+        res = []
+        for q in queries:
+            l, r = 0, len(p)-1
+            i = 0
+            while l<=r:
+                m = (l+r+1)//2
+                if q[0] >= p[m][0]:
+                    i = m
+                    l = m+1
+                else:
+                    r = m-1
+            if q[1] <= p[i][1]:
+                res.append(True)
             else:
-                result.append(True)
-        return result
+                res.append(False)
+        return res
+
