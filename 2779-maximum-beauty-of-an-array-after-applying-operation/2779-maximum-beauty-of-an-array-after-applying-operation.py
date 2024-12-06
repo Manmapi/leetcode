b6ba74453a3df14e7f2fbed35aa46e1b2b977cd9
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-06 09:57:13 +0700

    Time: 321 ms (27.51%), Space: 32.6 MB (16.19%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,33 +2,20 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
         n = len(nums)
-        if n <= 1:
-            return n
-        def canit(target):
-            stack = deque([[nums[0], 1]])
-            curr = 1
-            for num in nums[1:]:
-                if num - stack[0][0] <= 2 * k:
-                    if num > stack[-1][0]:
-                        stack.append([num, 1])
-                    elif num == stack[-1][0]:
-                        stack[-1][1] += 1
-                else:
-                    while stack and num - 2 * k > stack[0][0]:
-                        value, count = stack.popleft()
-                        curr -= count
+        stack = deque([[nums[0], 1]])
+        curr = 1
+        result = 1
+        for num in nums[1:]:
+            if num - stack[0][0] <= 2 * k:
+                if num > stack[-1][0]:
                     stack.append([num, 1])
-                curr += 1
-                if curr >= target:
-                    return True
-            return False
-        l = 0
-        r = n
-        while l <= r:
-            mid = (l + r) // 2
-            val = canit(mid)
-            if val: 
-                l = mid + 1
+                elif num == stack[-1][0]:
+                    stack[-1][1] += 1
             else:
-                r = mid - 1
-        return r
+                while stack and num - 2 * k > stack[0][0]:
+                    value, count = stack.popleft()
+                    curr -= count
+                stack.append([num, 1])
+            curr += 1
+            result = max(curr, result)
+        return result
