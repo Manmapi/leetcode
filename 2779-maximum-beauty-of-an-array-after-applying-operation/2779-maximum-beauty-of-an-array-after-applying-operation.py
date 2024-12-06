ac6010dd8c8e52d7ee8ddb5cc88cdba529748418
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-06 10:04:53 +0700

    Time: 224 ms (52.91%), Space: 28 MB (43.17%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,20 +2,11 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
         n = len(nums)
-        stack = deque([[nums[0], 1]])
-        curr = 1
+        left = 0
         result = 1
-        for num in nums[1:]:
-            if num - stack[0][0] <= 2 * k:
-                if num > stack[-1][0]:
-                    stack.append([num, 1])
-                elif num == stack[-1][0]:
-                    stack[-1][1] += 1
-            else:
-                while stack and num - 2 * k > stack[0][0]:
-                    value, count = stack.popleft()
-                    curr -= count
-                stack.append([num, 1])
-            curr += 1
-            result = max(curr, result)
+        for i in range(1, n):
+            num = nums[i]
+            while left < n and nums[left] + 2 * k < num:
+                left += 1
+            result = max(result, i - left + 1)
         return result
