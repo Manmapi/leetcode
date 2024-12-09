b2e85ddb39cad8bd27d3d42c14243541edc1ea32
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-09 10:38:18 +0700

    Time: 491 ms (18.17%), Space: 37.1 MB (5.3%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -5,30 +5,27 @@ class Solution:
             curr = count = 0
             for i in range(n):
                 x = bloomDay[i]
-                if x >= target:
+                if target >= x:
                     count += 1
                     if count >= k:
                         count -= k
                         curr += 1
                 else:
-                    count = 1
+                    count = 0
                 if curr >= m:
                     return True
             return False
         results = list(set(bloomDay))
         results.sort()
-        m = len(results)
         l = 0
-        r = m - 1
+        r = len(results) - 1
         flag = False
         while l <= r:
             mid = (l + r) // 2
             val = canit(results[mid])
             if val:
                 flag = True
-                l = mid + 1
-            else:
                 r = mid - 1
-        if l == m:
-            l -= 1
+            else:
+                l = mid + 1
         return results[l] if flag else - 1
