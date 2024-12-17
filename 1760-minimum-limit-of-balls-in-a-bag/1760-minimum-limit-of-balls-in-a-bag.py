2bbaa410f15f8573dfdcd11fc329f112d1e17bcd
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-17 08:40:58 +0700

    Time: 871 ms (16.02%), Space: 29.4 MB (54.49%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -1,18 +1,14 @@
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
         n = len(nums)
-        if n == 1:
-            if nums[0] % (maxOperations + 1) == 0:
-                return nums[0] // (maxOperations + 1)
-            else:
-                return nums[0] // (maxOperations + 1) + 1
-
         def canit(target):
             count = maxOperations
             i = 0
             for i in range(n): 
                 count -= ceil(nums[i] / target) - 1
-            return count >= 0
+                if count < 0:
+                    return False
+            return True
         
         l = 1
         r = max(nums)
