55b835ed886965015b16f853c2fdf27c03ba9de5
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-24 18:14:28 +0700

    Time: 51 ms (20.14%), Space: 37.5 MB (7.08%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,18 +1,15 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         n = len(nums)
-        suffix_table = [True] * n
-        suffix_table[-1] = False
-        max_ = nums[-1]
-        for i in range(n - 2, -1, -1):
-            if nums[i] >= max_:
-                suffix_table[i] = False
-                max_ = nums[i]
-        curr = nums[0]
+        a, b = nums[0], None
         for i in range(1, n):
-            if nums[i] > curr:
-                if suffix_table[i]:
-                    return True
-            else:
-                curr = nums[i]
+            if b is not None and nums[i] > b:
+                return True
+            if nums[i] > a:
+                if b is None:
+                    b = nums[i]
+                else:
+                    b = min(b, nums[i])
+            elif nums[i] < a:
+                a = nums[i]
         return False
