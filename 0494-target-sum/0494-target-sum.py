70c0b113c4210bea331012fcbd2697b964509845
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-25 08:52:34 +0700

    Time: 136 ms (72.37%), Space: 45.1 MB (5.49%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        @lru_cache(None)
+        def dp(value, index):
+            if index == n:
+                if value == target: 
+                    return 1
+                else:
+                    return 0
+            return dp(value - nums[index], index + 1) + dp(value + nums[index], index + 1)
+        return dp(0, 0)
