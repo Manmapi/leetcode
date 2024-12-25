39eb213e70744ab0678e0f38cbf68bbbcd3bff56
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-25 09:08:51 +0700

    Time: 126 ms (13.4%), Space: 18 MB (41.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        pointer = 0
-        for i in range(n):
+        l = len(nums)
+        i = 0
+        while i < l:
             if nums[i] == 0:
-                while pointer < n and (pointer <= i or nums[pointer] == 0):
-                    pointer += 1
-                if pointer >= n:
-                    return nums
-                nums[i], nums[pointer] = nums[pointer], nums[i]
-        return nums
+                nums.append(nums.pop(i))
+                i -= 1
+                l -= 1
+            i += 1
+        
