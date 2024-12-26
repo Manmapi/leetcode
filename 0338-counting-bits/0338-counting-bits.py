3b8ce37f4d336347d717edca6e8040e19de1dd83
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-26 17:24:33 +0700

    Time: 8 ms (44.07%), Space: 18.5 MB (14.48%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,8 +2,6 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         if n == 0:
             return [0]
-        if n == 1:
-            return [0, 1]
         result = [0, 1]
         for i in range(2, n + 1):
             value = i & 1
