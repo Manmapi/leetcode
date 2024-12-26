e7b5d8c3a8e6feb54ece0b376278a9f6c8ab507e
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-26 15:00:26 +0700

    Time: 215 ms (5.04%), Space: 51.9 MB (5.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -8,16 +8,9 @@ class Solution:
                 return max(0, n - i)
             if i >= n:
                 return max(0, m - j)
-            if word1[i] == word2[j]:
-                return min(
-                    dp(i + 1, j + 1),
-                    1 + dp(i + 1, j),
-                    1 + dp(i, j + 1)
-                )
-            else:
-                return min(
-                    1 + dp(i + 1, j + 1),
-                    1 + dp(i + 1, j),
-                    1 + dp(i, j + 1)
-                )
+            return min(
+                int(word1[i] != word2[j])+ dp(i + 1, j + 1),
+                1 + dp(i + 1, j),
+                1 + dp(i, j + 1)
+            )
         return dp(0, 0)
