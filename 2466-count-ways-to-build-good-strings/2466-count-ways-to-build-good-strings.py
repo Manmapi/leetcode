c209fbbd4da17d6131f63aba10295fbdbc20d55b
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-30 09:05:11 +0700

    Time: 186 ms (44.3%), Space: 139.3 MB (34.34%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -0,0 +1,13 @@
+MOD = 1_000_000_007
+
+class Solution:
+    def countGoodStrings(self, low: int, high: int, x: int, y: int) -> int:
+        @cache
+        def dp(value):
+            if value > high:
+                return 0
+            result = 0
+            if value >= low:
+                result += 1
+            return (result + dp(value + x) + dp(value + y)) % MOD
+        return dp(0) 
