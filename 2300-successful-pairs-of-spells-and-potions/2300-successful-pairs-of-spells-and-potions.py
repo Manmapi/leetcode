bde8ff7a73170b675484522c6974f6b124396939
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-30 09:44:56 +0700

    Time: 557 ms (38.4%), Space: 39.5 MB (12.62%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -2,7 +2,6 @@ class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         potions.sort()
         n = len(potions)
-        print(potions)
         def find_possible(spell):
             l, r = 0, n - 1
             while l <= r:
