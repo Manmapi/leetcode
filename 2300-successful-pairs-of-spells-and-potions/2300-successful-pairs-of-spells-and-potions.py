56f300943b0493bce7c6c8ec403cac2477bb0abf
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-30 09:45:21 +0700

    Time: 463 ms (63.46%), Space: 38.8 MB (25.38%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -3,11 +3,12 @@ class Solution:
         potions.sort()
         n = len(potions)
         def find_possible(spell):
+            target = ceil(success / spell) 
             l, r = 0, n - 1
             while l <= r:
                 mid = (l + r) // 2
-                value = spell * potions[mid]
-                if value >= success :
+                value = potions[mid]
+                if value >= target :
                     r = mid - 1
                 else:
                     l = mid + 1
