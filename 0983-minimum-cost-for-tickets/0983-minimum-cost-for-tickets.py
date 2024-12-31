f55b61954a8d60d6499fab6257628e35de14b45c
duy.tran <88496947+Manmapi@users.noreply.github.com>
2024-12-31 09:18:10 +0700

    Time: 31 ms (5.67%), Space: 20.1 MB (5.1%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -9,7 +9,6 @@ class Solution:
                 else:
                     return float("inf")
             results = []
-            new_last = last
             if last is None:
                 results.append(min(costs) + dp(None, current + 1))
                 results.append(dp(current, current + 1))
@@ -18,7 +17,8 @@ class Solution:
                     results.append(costs[1] + dp(None, current + 1))
                 if days[current] - days[last] < 30:
                     results.append(costs[2] + dp(None, current + 1))
-
+                if days[current] - days[last] >= 30:
+                    return float("inf")
                 results.append(dp(last, current + 1))
             result = min(results)
             return result
