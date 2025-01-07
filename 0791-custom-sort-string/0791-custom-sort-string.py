0b6c37cc295be82c09e26ab0c0ab84d78364d5e3
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-07 10:46:41 +0700

    Time: 3 ms (15.42%), Space: 17.9 MB (7.99%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -4,6 +4,5 @@ class Solution:
         for i, c in enumerate(order):
             m[c] = i
         def cmp(x, y):
-            return m.get(x, 999) - m.get(y, 999)
-        result = list(s)
-        return "".join(sorted(result, key=cmp_to_key(cmp)))
+            return m.get(x, 99) - m.get(y, 99)
+        return "".join(sorted(s, key=cmp_to_key(cmp)))
