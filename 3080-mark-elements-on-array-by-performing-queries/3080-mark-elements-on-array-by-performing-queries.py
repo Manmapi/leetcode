d7febe7127219175d406394aac9431e256c75c18
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-13 11:18:54 +0700

    Time: 1653 ms (88.89%), Space: 48.3 MB (82.05%) - LeetHub

--- 3080-mark-elements-on-array-by-performing-queries/3080-mark-elements-on-array-by-performing-queries.py
+++ 3080-mark-elements-on-array-by-performing-queries/3080-mark-elements-on-array-by-performing-queries.py
@@ -1,19 +1,22 @@
 class Solution:
     def unmarkedSumArray(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         q = [(num, i) for i, num in enumerate(nums)]
-        heapq.heapify(q)
-        marked = [False] * len(nums)
+        q.sort()
+        n = len(nums)
+        marked = [False] * n
         total = sum(nums)
         result = []
+        curr_index = 0
         for index, count in queries:
             if not marked[index]:
                 marked[index] = True
                 total -= nums[index]
-            while q and count > 0:
-                value, i = heapq.heappop(q)
+            while curr_index < n and count > 0:
+                value, i = q[curr_index]
                 if marked[i] == False:
                     marked[i] = True
                     count -= 1
                     total -= value
+                curr_index += 1
             result.append(total)
         return result
