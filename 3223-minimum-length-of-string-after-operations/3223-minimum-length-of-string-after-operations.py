b304ffcb635876c4d36493130c3ba79e5206a128
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-14 10:28:04 +0700

    Time: 294 ms (13.44%), Space: 18.8 MB (33.77%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -1,10 +1,17 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        counter = Counter(s)
-        result = 0
-        for v in counter.values():
-            if v % 2 == 0:
-                result += 2
+        arr = [0] * 26
+
+        L = len(s)
+
+        for c in s:
+            i = ord(c) - ord('a')
+
+            if arr[i] == 0:
+                arr[i] = 1
+            elif arr[i] == 1:
+                arr[i] = 2
             else:
-                result += 1
-        return result
+                arr[i] = 1
+        
+        return sum(arr)
