e01406b94985a4bbffacace4e055508fa2b57852
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-14 10:30:24 +0700

    Time: 259 ms (17.05%), Space: 19 MB (15.41%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -6,12 +6,13 @@ class Solution:
 
         for c in s:
             i = ord(c) - ord('a')
-
-            if arr[i] == 0:
-                arr[i] = 1
-            elif arr[i] == 1:
-                arr[i] = 2
+            arr[i] += 1
+        result = 0
+        for num in arr:
+            if num == 0:
+                continue
+            if num % 2:
+                result += 1
             else:
-                arr[i] = 1
-        
-        return sum(arr)
+                result += 2
+        return result
