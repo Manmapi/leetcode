ce041371c43b0f135cac1b7efd65ef915a88b662
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-14 12:03:43 +0700

    Time: 414 ms (46.94%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -11,14 +11,10 @@ with unique_position as (
     select tiv_2015 from Insurance
     group by tiv_2015
     having count(1) > 1
-), tiv as (
-    select i1.tiv_2015, round(cast(sum(tiv_2016) as numeric), 2) as tiv_2016 from Insurance i1
-    join unique_company u
-    on i1.pid = u.pid
-    join same_tiv_15 s
-    on s.tiv_2015 = i1.tiv_2015
-    group by i1.tiv_2015
 )
 -- select * from unique_company
-select sum(tiv_2016) as tiv_2016 from tiv
-
+select round(cast(sum(tiv_2016) as numeric), 2) as tiv_2016 from Insurance i1
+join unique_company u
+on i1.pid = u.pid
+join same_tiv_15 s
+on s.tiv_2015 = i1.tiv_2015
