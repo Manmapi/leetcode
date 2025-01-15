02f762dfe99b01abd3f355129c003f90ee8b25f1
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-15 14:18:29 +0700

    Time: 130 ms (52.78%), Space: 18.3 MB (17.21%) - LeetHub

---
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minTimeToReach(self, A: List[List[int]]) -> int:
+        n = len(A)
+        m = len(A[0])
+        costs = [[float('inf')] * m for _ in range(n)]
+        costs[0][0] = 0
+
+        q = [(0, 0, 0)]
+        next_ = {(0, 1), (0, -1), (1, 0), (-1, 0)}
+        while q:
+            cost, x, y = heapq.heappop(q)
+            if x == n - 1 and y == m - 1:
+                return cost
+            for i, j in next_:
+                x_ = x + i
+                y_ = y + j
+                if x_ < 0 or x_ >= n or y_ < 0 or y_ >= m:
+                    continue
+                n_cost = max(A[x_][y_] + 1, cost + 1)
+                if n_cost < costs[x_][y_]:
+                    heapq.heappush(q, (n_cost, x_, y_))
+                    costs[x_][y_] = n_cost
+        return costs[-1][-1]
+            
