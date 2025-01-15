31af0552f17116aa8eef2f4e35324c2d981f1ed1
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-15 14:29:23 +0700

    Time: 1633 ms (77.31%), Space: 88 MB (75.18%) - LeetHub

--- 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.py
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.py
@@ -4,22 +4,21 @@ class Solution:
         m = len(A[0])
         costs = [[float('inf')] * m for _ in range(n)]
         costs[0][0] = 0
-
         q = [(0, 0, 0)]
         next_ = {(0, 1), (0, -1), (1, 0), (-1, 0)}
         while q:
             cost,x, y = heapq.heappop(q)
             if x == n - 1 and y == m - 1:
                 return cost
-            fine = 1
+            fee = 1
             if (x + y) % 2:
-                fine += 1
+                fee += 1
             for i, j in next_:
                 x_ = x + i
                 y_ = y + j
                 if x_ < 0 or x_ >= n or y_ < 0 or y_ >= m:
                     continue
-                n_cost = max(A[x_][y_] + fine, cost + fine)
+                n_cost = max(A[x_][y_] + fee, cost + fee)
                 if n_cost < costs[x_][y_]:
                     heapq.heappush(q, (n_cost, x_, y_))
                     costs[x_][y_] = n_cost
