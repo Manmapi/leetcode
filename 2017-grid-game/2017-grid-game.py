3cb89a07b68ceeda6aa8fe93327b4c8b7a39e2ac
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-21 09:11:11 +0700

    Time: 86 ms (76.07%), Space: 28.7 MB (82.3%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,18 +1,13 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         n = len(grid[0])
-        
-        total_up = 0
-        pre_up = []
-        for num in grid[0]:
-            total_up += num
-            pre_up.append(total_up)
-        
         result = float('inf')
+        up = 0
         down = 0
+        up =  sum(grid[0])
         for i in range(n):
-            up = total_up - pre_up[i]
             if i > 0:
                 down += grid[1][i - 1]
+            up -= grid[0][i]
             result = min(result, max(up, down))
         return result
