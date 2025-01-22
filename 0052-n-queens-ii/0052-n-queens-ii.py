041d45388fe5cf8dab2d617ee632468b42318bca
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-22 11:59:51 +0700

    Time: 15 ms (34.49%), Space: 18 MB (17.41%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         def check(ver, crossx, crossy, i, j): return not (1 << j & ver or (1 << (i + j)) & crossx or (1 << (j - i + n)) & crossy)
-
+        
         def dfs(ver, crossx, crossy, i):
             if i == n - 1: return sum([1 for j in range(n) if check(ver, crossx, crossy, i, j)])
             return sum([
