0d8b8a083099770b51c46e151a06f74b77e792d0
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-22 13:45:49 +0700

    Time: 214 ms (29.93%), Space: 39.1 MB (29.2%) - LeetHub

--- 1001-grid-illumination/1001-grid-illumination.py
+++ 1001-grid-illumination/1001-grid-illumination.py
@@ -4,41 +4,36 @@ class Solution:
         ver = defaultdict(int)
         crossx = defaultdict(int)
         crossy = defaultdict(int)
-        visited = set()
         m = {}
+
         for x, y in lamps:
-            if (x, y) in visited:
+            if (x, y) in m:
                 continue
             m[(x, y)] = True
-            visited.add((x, y))
             hor[x] += 1
             ver[y] += 1
             crossx[x + y] += 1
             crossy[y - x] += 1
-
-        def turnoff(x, y):
-            if not m.get((x, y)):
-                return
-            hor[x] -= 1
-            ver[y] -= 1
-            crossx[x + y] -= 1
-            crossy[y - x] -= 1
-            m[(x, y)] = False
     
         def turnoffAdj(x, y):
-            turnoff(x, y)
-            for i, j in {(1, 1), (0, 1), (1, 0), (-1, -1), (0, -1), (-1, 0), (-1, 1), (1, -1)}:
+            for i, j in {(1, 1), (0, 1), (1, 0), (-1, -1), (0, -1), (-1, 0), (-1, 1), (1, -1), (0, 0)}:
                 h, k = x + i, y + j
                 if h < 0 or h >= n or k < 0 or k >= n:
                     continue
-                turnoff(h, k)
+                if not m.get((h, k)):
+                    continue
+                hor[h] -= 1
+                ver[k] -= 1
+                crossx[h + k] -= 1
+                crossy[k - h] -= 1
+                m[(h, k)] = False
+
         def isIlluminated(x, y):
             return int(hor[x] > 0 or ver[y] > 0 or crossx[x + y] > 0 or crossy[y - x] > 0)  
             
         result = []
         for x, y in queries:
             result.append(isIlluminated(x, y))
-
             turnoffAdj(x, y)
         
         return result
