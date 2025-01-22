e834429eef7cd89052a31f518c05fc9c1415ec91
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-22 13:53:53 +0700

    Time: 207 ms (33.58%), Space: 38.7 MB (32.85%) - LeetHub

--- 1001-grid-illumination/1001-grid-illumination.py
+++ 1001-grid-illumination/1001-grid-illumination.py
@@ -13,9 +13,11 @@ class Solution:
             hor[x] += 1
             ver[y] += 1
             crossx[x + y] += 1
-            crossy[y - x] += 1
-    
-        def turnoffAdj(x, y):
+            crossy[y - x] += 1            
+            
+        result = []
+        for x, y in queries:
+            result.append(int(hor[x] > 0 or ver[y] > 0 or crossx[x + y] > 0 or crossy[y - x] > 0))
             for i, j in {(1, 1), (0, 1), (1, 0), (-1, -1), (0, -1), (-1, 0), (-1, 1), (1, -1), (0, 0)}:
                 h, k = x + i, y + j
                 if h < 0 or h >= n or k < 0 or k >= n:
@@ -27,14 +29,6 @@ class Solution:
                 crossx[h + k] -= 1
                 crossy[k - h] -= 1
                 m[(h, k)] = False
-
-        def isIlluminated(x, y):
-            return int(hor[x] > 0 or ver[y] > 0 or crossx[x + y] > 0 or crossy[y - x] > 0)  
-            
-        result = []
-        for x, y in queries:
-            result.append(isIlluminated(x, y))
-            turnoffAdj(x, y)
         
         return result
             
