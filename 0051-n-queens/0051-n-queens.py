eb11d6886cb80b871b648c338bc31044626327a6
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-22 11:43:02 +0700

    Time: 14 ms (48.5%), Space: 18.4 MB (22.07%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -7,22 +7,17 @@ class Solution:
         
         def dfs(ver, hor, crossx, crossy, i):
             if i == n - 1:
-                base = ["."] * n
                 return [
-                    ['.' * j + 'Q' + '.' * (n - j - 1)]
-                    for j in range(n)
+                    ['.' * j + 'Q' + '.' * (n - j - 1)] for j in range(n)
                     if check(ver, hor, crossx, crossy, i, j)
                 ]
             result = []
-            base = ["."] * n
             new_hor = (1 < i) | hor
             for j in range(n):
-                curr = base.copy()
-                curr[j] = "Q"
                 if check(ver, hor, crossx, crossy, i, j):
                     next_result = dfs((1 << j) | ver, new_hor, 1 << (i + j) | crossx, 1 << (j - i + n) | crossy, i + 1)
                     for nr in next_result:
-                        result.append(nr + ["".join(curr)])
+                        result.append(nr + ['.' * j + 'Q' + '.' * (n - j - 1)])
             return result
         return dfs(0, 0, 0, 0, 0)
 
