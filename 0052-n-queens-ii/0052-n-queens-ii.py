0acfa337046a9517aac0dfd4e2b13594bce51f2c
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-22 11:53:45 +0700

    Time: 18 ms (30.64%), Space: 17.6 MB (66.66%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -3,16 +3,11 @@ class Solution:
         def check(ver, crossx, crossy, i, j):
             return not (1 << j & ver or (1 << (i + j)) & crossx or (1 << (j - i + n)) & crossy)
 
-        @cache  
         def dfs(ver, crossx, crossy, i):
-            if i == n - 1: 
-                return sum([
-                    1 for j in range(n) if check(ver, crossx, crossy, i, j)
-                ])
-            result = 0
-            for j in range(n):
-                if check(ver, crossx, crossy, i, j):
-                    result += dfs((1 << j) | ver, 1 << (i + j) | crossx, 1 << (j - i + n) | crossy, i + 1)
-            return result
+            if i == n - 1: return sum([1 for j in range(n) if check(ver, crossx, crossy, i, j)])
+            return sum([
+                dfs((1 << j) | ver, 1 << (i + j) | crossx, 1 << (j - i + n) | crossy, i + 1)
+                for j in range(n) if check(ver, crossx, crossy, i, j)
+            ])
         return dfs(0, 0, 0, 0)
         
