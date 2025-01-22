bf7f087ec80fcd744a012d59b8c06d18b3a8514f
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-22 11:51:53 +0700

    Time: 11 ms (60.6%), Space: 18 MB (17.41%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,23 +1,17 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:        
+    def totalNQueens(self, n: int) -> int:
         def check(ver, crossx, crossy, i, j):
             return not (1 << j & ver or (1 << (i + j)) & crossx or (1 << (j - i + n)) & crossy)
                 
         def dfs(ver, crossx, crossy, i):
             if i == n - 1: 
-                return [
-                    ['.' * j + 'Q' + '.' * (n - j - 1)] for j in range(n)
-                    if check(ver, crossx, crossy, i, j)
-                ]
-            result = []
+                return sum([
+                    1 for j in range(n) if check(ver, crossx, crossy, i, j)
+                ])
+            result = 0
             for j in range(n):
                 if check(ver, crossx, crossy, i, j):
-                    next_result = dfs((1 << j) | ver, 1 << (i + j) | crossx, 1 << (j - i + n) | crossy, i + 1)
-                    for nr in next_result:
-                        result.append(nr + ['.' * j + 'Q' + '.' * (n - j - 1)])
+                    result += dfs((1 << j) | ver, 1 << (i + j) | crossx, 1 << (j - i + n) | crossy, i + 1)
             return result
         return dfs(0, 0, 0, 0)
-    def totalNQueens(self, n: int) -> int:
-        return len(self.solveNQueens(n))
-        
         
