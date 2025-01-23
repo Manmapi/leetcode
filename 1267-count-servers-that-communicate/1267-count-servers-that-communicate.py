6013fa9c2020353b3198fc8c04adabe9c9723804
duy.tran <88496947+Manmapi@users.noreply.github.com>
2025-01-23 10:06:18 +0700

    Time: 46 ms (14.23%), Space: 19.7 MB (22.47%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -4,14 +4,14 @@ class Solution:
         m = len(grid[0])
         ver = [0] * m
         hor = [0] * n
-        
         for i in range(n):
             for j in range(m):
                 ver[j] += grid[i][j]
                 hor[i] += grid[i][j]
-
+                
         result = 0
         for i in range(n):
             for j in range(m):
-                result += grid[i][j] and (ver[j] > 1 or hor[i] > 1)
+                if grid[i][j] and (ver[j] > 1 or hor[i] > 1):
+                    result += 1
         return result
